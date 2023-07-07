14060b25cb06fdfdbf8da2d3801139a05b72ea1a
Yussy23 <yousra.dribi15@gmail.com>
2023-07-07 16:05:39 -0700

    mandatory tasks

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,46 +1,30 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
+#include "hash_tables.h"
 
 /**
- * struct hash_node_s - Node of a hash table
- *
- * @key: The key, string
- * The key is unique in the HashTable
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
+ * hash_table_delete - function deletes a hash table.
+ * @ht: A pointer parameter to a hash table.
  */
-typedef struct hash_node_s
+void hash_table_delete(hash_table_t *ht)
 {
-	char *key;
-	char *value;
-	struct hash_node_s *next;
-} hash_node_t;
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
 
-/**
- * struct hash_table_s - Hash table data structure
- *
- * @size: The size of the array
- * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a Chaining collision handling
- */
-typedef struct hash_table_s
-{
-	unsigned long int size;
-	hash_node_t **array;
-} hash_table_t;
-
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const hash_table_t *ht, const char *key);
-void hash_table_print(const hash_table_t *ht);
-void hash_table_delete(hash_table_t *ht);
-
-
-#endif
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
